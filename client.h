4e290ca71a3c38bc586bc2bdfea12bdf2c37fc4b
Benjamin Villa <ywqqqq@gmail.com>
2021-03-31 22:30:14 +0200

    cleaning

--- client.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLIENT_H
-#define CLIENT_H
-#include <stdio.h>
-#include <stdlib.h>
-#include "communication.h"
-#endif
--- serveur.h
+++
@@ -1,6 +0,0 @@
-#ifndef SERVEUR_H
-#define SERVEUR_H
-#include <stdio.h>
-#include <stdlib.h>
-#include "communication.h"
-#endif

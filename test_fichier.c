6d0ca05660e535e58b5fcdbdd1a034fa80fc268c
Ophelie-K <ophelie.kitty20@gmail.com>
2021-03-31 17:16:44 +0200

    Opendir

--- test_fichier.c
+++ test_fichier.c
@@ -3,20 +3,52 @@ Il est également une référence pour savoir comment s'utilisent les fonctions 
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include "carte.h"
 #include "scenario.h"
 #include "fichier.h"
 
 int main(int argc, char* argv[]){
-  carte * c = malloc(sizeof(carte));
-  scenario * s = malloc(sizeof(scenario));
-  if(argc != 2){
-    printf("Veuillez indiquer le chemin fichier de carte à lire");
+  int retour;
+  carte *c = malloc(sizeof(carte));
+  scenario *s = malloc(sizeof(scenario));
+  DIR *repCarte;
+  DIR *repScenario;
+  struct dirent *carte;
+  struct dirent *scenario; 
+  repCarte = opendir("resources/cartes");
+  if(repCarte == NULL){
+    fprintf(stderr,"Erreur lors de l'ouverture du repertoire des cartes");
+    perror("OPENDIR");
     exit(EXIT_FAILURE);
   }
-  lire_carte(argv[1], c);
+  repScenario = opendir("resources/scenarios");
+    if(repScenario == NULL){
+    fprintf(stderr,"Erreur lors de l'ouverture du repertoire des scenarios");
+    perror("OPENDIR");
+    exit(EXIT_FAILURE);
+  }
+  carte = readdir(repCarte);
+  printf("%s", carte -> d_name);
+  lire_carte("resources/cartes/easy.bin", c);
   printf("taille : %d\n", c-> taille);
-  printf("description : %s\n", c-> description);
-  printf("ligne 1 : %d %d %d %d ...\n", c-> plateau[0][0], c-> plateau[0][1], c-> plateau[0][2], c-> plateau[0][3]);
+  printf("description : %s", c-> description);
+  lire_scenario("resources/scenarios/infini.bin", s);
+  printf("taille : %d\n", s-> taille);
+  printf("description : %s\n", s-> description);
+  retour = closedir(repCarte);
+  if(retour !=0){
+    fprintf(stderr,"Erreur lors de la fermeture du repertoire de cartes");
+    perror("CLOSEDIR");
+    exit(EXIT_FAILURE);
+  }
+  retour = closedir(repScenario);
+  if(retour !=0){
+    fprintf(stderr,"Erreur lors de la fermeture du repertoire de scenarios");
+    perror("CLOSEDIR");
+    exit(EXIT_FAILURE);
+  }
+  exit(EXIT_SUCCESS);
 }
 

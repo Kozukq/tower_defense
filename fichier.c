b1eee57b5bcd25ef76ffaf5e0620cd9d6465a483
Ophelie-K <ophelie.kitty20@gmail.com>
2021-03-31 16:25:04 +0200

    printf remplaces par des fprintf

--- fichier.c
+++ fichier.c
@@ -7,32 +7,30 @@ void lire_carte(char * filename, carte * c){
   unsigned char bytesLigne[sizeof(unsigned char)*TAILLE_LIGNE_CARTE];
   fd = open(filename, O_RDONLY);
   if(fd == -1){
-    printf("Erreur lors de l'ouverture de la carte\n");
+    fprintf(stderr,"Erreur lors de l'ouverture de la carte\n");
     perror("OPEN");
     exit(EXIT_FAILURE);
   }
   retour = read(fd, bytesTaille, sizeof(size_t));
   if(retour < 0){
-    printf("Erreur lors de la lecture des octets dans le fichier\n");
+    fprintf(stderr,"Erreur lors de la lecture des octets dans le fichier\n");
     perror("READ");
     exit(EXIT_FAILURE);
   }
   size_t taille = (int) bytesTaille[0];
-  printf("taille : %d", taille);
   c-> taille = taille;
   retour = read(fd, bytesDesc, taille * sizeof(char));
   if(retour < 0){
-    printf("Erreur lors de la lecture des octets dans le fichier\n");
+    fprintf(stderr,"Erreur lors de la lecture des octets dans le fichier\n");
     perror("READ");
     exit(EXIT_FAILURE);
   }
-  printf("description : %s", bytesDesc);
   c -> description = malloc(sizeof(char)*taille);
   memcpy(c->description, bytesDesc,sizeof(char)*taille);
   for(i = 0; i < 15; i = i+1){
     retour = read(fd, bytesLigne, sizeof(unsigned char)*TAILLE_LIGNE_CARTE);
     if(retour < 0){
-      printf("Erreur lors de la lecture des octets dans le fichier\n");
+      fprintf(stderr,"Erreur lors de la lecture des octets dans le fichier\n");
       perror("READ");
       exit(EXIT_FAILURE);
     }
@@ -50,27 +48,25 @@ void lire_scenario(char * filename, scenario * s){
   unsigned char bytesDesc[255];
   fd = open(filename, O_RDONLY);
   if(fd == -1){
-    printf("Erreur lors de l'ouverture du scénario\n");
+    fprintf(stderr,"Erreur lors de l'ouverture du scénario\n");
     perror("OPEN");
     exit(EXIT_FAILURE);
   }
   retour = read(fd, bytesTaille, sizeof(size_t));
   if(retour < 0){
-    printf("Erreur lors de la lecture des octets dans le fichier\n");
+    fprintf(stderr,"Erreur lors de la lecture des octets dans le fichier\n");
     perror("READ");
     exit(EXIT_FAILURE);
   }
   size_t taille = (int) bytesTaille[0];
-  printf("taille : %d", taille);
   s-> taille = taille;
   
   retour = read(fd, bytesDesc, taille * sizeof(char));
   if(retour < 0){
-    printf("Erreur lors de la lecture des octets dans le fichier\n");
+    fprintf(stderr,"Erreur lors de la lecture des octets dans le fichier\n");
     perror("READ");
     exit(EXIT_FAILURE);
   }
-  printf("description : %s", bytesDesc);
   s -> description = malloc(sizeof(char) * taille);
   memcpy(s->description, bytesDesc,sizeof(char)*taille);
 }

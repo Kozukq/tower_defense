ea79ff74b9f55615288c93649dc18a3ee6479c86
Benjamin Villa <ywqqqq@gmail.com>
2021-04-07 19:18:51 +0200

    hotfix

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define _DEFAULT_SOURCE
+#define _POSIX_C_SOURCE 200809L
 #define DEBUG 0
 
 #include <stdint.h> /* nécessaire pour gérer les types à taille fixe sur Linux apparemment */
